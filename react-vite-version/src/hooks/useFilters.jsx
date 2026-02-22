e54708d5d33283b6eba3035c628e1d48a135ffbd
andresvaldezt <andresvaldezt@gmail.com>
2026-02-21 19:37:55 -0600

    Enhance job search functionality by adding textToFilter prop and updating useFilters for improved state management

--- react-vite-version/src/hooks/useFilters.jsx
+++ react-vite-version/src/hooks/useFilters.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { useRouter } from "../hooks/useRouter"
 
 const RESULTS_PER_PAGE = 4;
 const initialFilters = {
@@ -14,12 +15,32 @@ export function useFilters(){
     //estado actual, un metodo para actualizar el estado
     //y un estado inicial.
 
-    const [filters, setFilters] = useState(initialFilters)
-    const [textToFilter, setTextToFilter ] = useState('')
-    const [currentPage, setCurrentPage ] = useState(1)
+    const [filters, setFilters] = useState(() =>{
+        const params = new URLSearchParams(window.location.search)
+        return {
+            technology: params.get('technology') || '',
+            location: params.get('type') || '',
+            experiencielevel: params.get('level') || ''
+        }
+    })
+
+    const [textToFilter, setTextToFilter ] = useState(() =>{
+        const params = new URLSearchParams(window.location.search)
+        return params.get('text') || ''
+    })
+
+    const [currentPage, setCurrentPage ] = useState(() =>{
+        const params = new URLSearchParams(window.location.search)
+        const page = Number(params.get('page'))
+        // Si NO es NaN y es mayor a 0, usa ese número. Si no, usa 1.
+        return (!Number.isNaN(page) && page > 0) ? page : 1;
+    })
+
     const [jobs, setJobs] = useState([])
     const [total, setTotal] = useState(0)
     const [loading, setLoading] = useState(true)
+
+    const { navigateTo } = useRouter()
 
     const isFiltered = Object.values(filters).some(v => 
         v !== null && v !== undefined && v.toString().trim() !== ''
@@ -60,6 +81,22 @@ export function useFilters(){
         fetchJobs()
     }, [filters, textToFilter, currentPage])
 
+    useEffect(() =>{
+        const params = new URLSearchParams()
+
+        if (textToFilter) params.append('text', textToFilter)
+        if (filters.technology) params.append('technology', filters.technology)
+        if (filters.location) params.append('type', filters.location)
+        if (filters.experiencielevel) params.append('experiencielevel', filters.experiencielevel)
+        if (currentPage > 1) params.append('page', currentPage)
+        
+        const newUrl = params.toString()
+            ? `${window.location.pathname}?${params.toString()}`
+            : window.location.pathname
+
+        navigateTo(newUrl)
+    }, [filters, textToFilter, currentPage, navigateTo])
+
     const totalPages = Math.ceil(total / RESULTS_PER_PAGE);
 
     const handlePageChange = (page) => {
@@ -95,6 +132,7 @@ export function useFilters(){
         handleTextFilter,
         isFiltered,
         handleClearFilters,
-        setFilters
+        setFilters,
+        textToFilter
     }
 }
